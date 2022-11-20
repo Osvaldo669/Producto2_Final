e747bf1f4e28935ac9f31e88ea6b161d805d0de6
mcmenamc <mcmena636@gmail.com>
2022-11-20 13:22:54 -0600

    WEB API login

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -16,8 +16,10 @@ namespace WebAPI
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{controller}/{action}/{id}/{id2}",
+                defaults: new { id = RouteParameter.Optional,
+                    id2 = RouteParameter.Optional
+                }
             );
         }
     }
